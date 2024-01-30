6ed66ce50f2808f47f108695335ab444f8ce8392
milicapet <milicap3108@gmail.com>
2024-01-30 23:32:49 +0100

    kreirane klase za factory

--- laravelapp/database/factories/UserFactory.php
+++ laravelapp/database/factories/UserFactory.php
@@ -18,11 +18,18 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'age' => $this->faker->numberBetween(18, 65),
+            'gender' => $this->faker->randomElement(['male', 'female', 'other']),
+            'weight' => $this->faker->numberBetween(40, 150),
+            'height' => $this->faker->numberBetween(140, 210),
+            'activity_level' => $this->faker->randomElement(['sedentary', 'lightly_active', 'moderately_active', 'very_active']),
+            'dietary_restrictions' => $this->faker->optional()->text(),
+            'notes' => $this->faker->text(),
         ];
     }
 
