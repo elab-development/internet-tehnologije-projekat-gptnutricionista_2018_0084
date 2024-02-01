bb03e72b7a58818d84105a270aa62f6fb0f43ae0
mariapopov <thereisslimshadyinallofus@gmail.com>
2024-02-01 20:18:41 +0100

    dodata paginacija i filter

--- reactapp/src/komponente/meals/Meals.jsx
+++ reactapp/src/komponente/meals/Meals.jsx
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import MealRow from './MealRow';  
-
-const Meals = () => {
-  const navigate = useNavigate();
-  const [meals, setMeals] = useState([]);
-  const palette = {
+import ReactPaginate from 'react-paginate';
+const palette = {  //paleta boja koja se koristi za css
     primary: '#c9c9ee',
     secondary: '#baabbd',
     tertiary: '#9f838c',
     quaternary: '#8d7471',
     quinary: '#816f68',
   };
+const Meals = () => {
+  const navigate = useNavigate();
+  const [meals, setMeals] = useState([]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const mealsPerPage = 5; // Broj obroka po stranici
+  const pageCount = Math.ceil(meals.length / mealsPerPage);
   useEffect(() => {
     const fetchMeals = async () => {
       try {
@@ -27,9 +30,24 @@ const Meals = () => {
     fetchMeals();
   }, []);
 
+  const handlePageClick = (data) => {  //poziva se kada korisnik klikne dugme za paginaciju
+    setCurrentPage(data.selected);
+  };
+ //obracunava od kog do kog indeksa treba da prikazemo elemente za paginaciju
+  const offset = currentPage * mealsPerPage;
+  const currentMeals = meals.slice(offset, offset + mealsPerPage);
   return (
     <div className="meals-container" style={{ backgroundColor: palette.primary }}>
       <h1 style={{ color: palette.quaternary }}>Obroci</h1>
+      <ReactPaginate
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName="pagination"
+        activeClassName="active"
+        previousLabel="<"
+        nextLabel=">"
+        />
+
       <table className="meals-table">
         <thead>
           <tr>
@@ -40,7 +58,7 @@ const Meals = () => {
           </tr>
         </thead>
         <tbody>
-          {meals.map((meal) => (
+          {currentMeals.map((meal) => (
             <MealRow
               key={meal.id}
               meal={meal}
